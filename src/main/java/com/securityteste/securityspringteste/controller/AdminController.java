8c4cfb28817bd3873adaa916b6b79235b125e835
Danniel Albuquerque <danniel@virtual.ufc.br>
2021-11-22 14:45:29 -0300

    ajustando a resposta

--- src/main/java/com/securityteste/securityspringteste/controller/AdminController.java
+++ src/main/java/com/securityteste/securityspringteste/controller/AdminController.java
@@ -1,5 +1,9 @@
 package com.securityteste.securityspringteste.controller;
 
+import com.securityteste.securityspringteste.response.ResponseHandler;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,8 +15,12 @@ public class AdminController {
 
     @GetMapping
     @PreAuthorize("hasAnyRole('ADMIN')")
-    public String index(){
-        return "Área Administrativa!";
+    public ResponseEntity<Object> index(){
+        try {
+            return ResponseHandler.generateResponse("Área Administrativa!", HttpStatus.OK, null);            
+        } catch (Exception e) {
+            return ResponseHandler.generateResponse(e.getMessage(), HttpStatus.MULTI_STATUS, null);
+        }
     }
     
 }
