09b1e40533cb67239a09a93912bac8709a0ab495
Danniel Albuquerque <danniel@virtual.ufc.br>
2021-11-29 11:15:51 -0300

    ajustes

--- src/main/java/com/securityteste/securityspringteste/filter/TokenAuthenticationFilter.java
+++ src/main/java/com/securityteste/securityspringteste/filter/TokenAuthenticationFilter.java
@@ -13,7 +13,7 @@ import com.securityteste.securityspringteste.service.auth.UserDetailsServiceImpl
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import io.jsonwebtoken.ExpiredJwtException;
