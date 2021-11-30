4e7102a12ba37d9f06ef882e48ae245e0fc91e2b
Danniel Albuquerque <danniel@virtual.ufc.br>
2021-11-30 16:21:18 -0300

    ajustes

--- src/main/java/com/securityteste/securityspringteste/SecurityspringtesteApplication.java
+++ src/main/java/com/securityteste/securityspringteste/SecurityspringtesteApplication.java
@@ -2,14 +2,12 @@ package com.securityteste.securityspringteste;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class SecurityspringtesteApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SecurityspringtesteApplication.class, args);
-		//System.out.println(new BCryptPasswordEncoder().encode("123"));
+		SpringApplication.run(SecurityspringtesteApplication.class, args);		
 	}
 
 }
