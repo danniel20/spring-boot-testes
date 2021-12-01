46f8ea326dedc3049c8ae52c9f19d6a05d6487d7
Danniel Albuquerque <danniel@virtual.ufc.br>
2021-12-01 14:38:04 -0300

    ajuste

--- src/main/java/com/securityteste/securityspringteste/config/security/SecurityConfig.java
+++ src/main/java/com/securityteste/securityspringteste/config/security/SecurityConfig.java
@@ -35,7 +35,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        System.out.println("método configure (HttpSecurity ...)");
         http
             .authorizeRequests()
             .antMatchers(HttpMethod.POST, "/auth").permitAll()
