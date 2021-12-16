e399c80841fa701d4d20e92eef57d288d5de55ff
Danniel Albuquerque <danniel@virtual.ufc.br>
2021-12-16 15:59:49 -0300

    ajustes

--- src/main/java/com/securityteste/securityspringteste/controller/AuthenticationController.java
+++ src/main/java/com/securityteste/securityspringteste/controller/AuthenticationController.java
@@ -1,4 +1,4 @@
-package com.securityteste.securityspringteste.controller.auth;
+package com.securityteste.securityspringteste.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
