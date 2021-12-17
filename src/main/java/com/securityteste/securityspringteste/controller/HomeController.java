5428ac4d92fb5c8f0d8f0a8812cfcb3273c0c369
Danniel Albuquerque <danniel@virtual.ufc.br>
2021-12-17 14:48:09 -0300

    ajustes

--- src/main/java/com/securityteste/securityspringteste/controller/HomeController.java
+++ src/main/java/com/securityteste/securityspringteste/controller/HomeController.java
@@ -1,8 +1,11 @@
 package com.securityteste.securityspringteste.controller;
 
+import java.time.LocalDate;
+
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -17,6 +20,7 @@ public class HomeController {
     public ModelAndView index(){
         ModelAndView mv = new ModelAndView("home");
         mv.addObject("message", "Seja bem-vindo!");
+		mv.addObject("data", LocalDate.now());
         return mv;
     }
 
@@ -25,4 +29,9 @@ public class HomeController {
     public String admin(){
         return "admin";
     }
+
+	@ModelAttribute("dataAtual")
+	public LocalDate dataAtual(){
+		return LocalDate.now();
+	}
 }
--- src/main/java/com/securityteste/securityspringteste/controller/UsuarioController.java
+++ src/main/java/com/securityteste/securityspringteste/controller/UsuarioController.java
@@ -1,18 +1,39 @@
 package com.securityteste.securityspringteste.controller;
 
+import java.time.LocalDate;
+import java.util.List;
+
+import com.securityteste.securityspringteste.model.Usuario;
+import com.securityteste.securityspringteste.service.usuarios.UsuarioService;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/usuarios")
 public class UsuarioController {
 
+	@Autowired
+	private UsuarioService usuarioService;
+
 	@GetMapping
 	@PreAuthorize("hasAnyRole('ADMIN')")
-	public String index(){
-		return "usuarios/index";
+	public ModelAndView index(){
+		List<Usuario> usuariosList = this.usuarioService.buscarTodos();
+
+		ModelAndView mv = new ModelAndView("usuarios/index");
+        mv.addObject("usuarios", usuariosList);
+        return mv;
+	}
+
+	@ModelAttribute("dataAtual")
+	public LocalDate dataAtual(){
+		return LocalDate.now();
 	}
 
 }
