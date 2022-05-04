82ad6649f652479eb3a798ed672dccf0d3e88f88
Danniel Albuquerque <danniel.master@gmail.com>
2022-05-04 11:22:21 -0300

    ajustes

--- src/main/java/com/securityteste/securityspringteste/config/database/EmptyMigrationStrategyConfig.java
+++ src/main/java/com/securityteste/securityspringteste/config/database/EmptyMigrationStrategyConfig.java
@@ -1,18 +1,28 @@
 package com.securityteste.securityspringteste.config.database;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 
 @Configuration
-@Profile("development")
 public class EmptyMigrationStrategyConfig {
+
+	@Value("${spring.profiles.active}")
+	private String environment;
 
 	@Bean
 	public FlywayMigrationStrategy flywayMigrationStrategy() {
-		return flyway -> {
-			// do nothing
-		};
+
+		if (this.environment.equals("development")) {
+			return flyway -> {
+				// do nothing
+			};
+		}
+		else{
+			return flyway -> {
+				flyway.migrate();
+			};
+		}
 	}
 }
