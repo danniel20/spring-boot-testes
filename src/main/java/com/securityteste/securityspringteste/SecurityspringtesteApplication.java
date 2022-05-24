2fb0b5350f846456284bbb6866f1489e2aa8edde
Danniel Albuquerque <danniel.master@gmail.com>
2022-05-24 11:19:02 -0300

    ajustes

--- src/main/java/com/securityteste/securityspringteste/SecurityspringtesteApplication.java
+++ src/main/java/com/securityteste/securityspringteste/SecurityspringtesteApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SecurityspringtesteApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SecurityspringtesteApplication.class, args);		
+		SpringApplication.run(SecurityspringtesteApplication.class, args);
 	}
 
 }
